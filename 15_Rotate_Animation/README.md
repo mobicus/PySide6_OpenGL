5fe39bcd2df3b28c9ee301175015a37f61756643
Binulal Narayanan <binulal.narayanan@umontreal.ca>
2022-01-24 18:45:25 -0400

    transformations

---
+++ 15_Rotate_Animation/README.md
@@ -0,0 +1,1 @@
+<a href="https://iconscout.com/icons/play" target="_blank">Play Icon</a> by <a href="https://iconscout.com/contributors/ibm-design">IBM-Design</a> on <a href="https://iconscout.com">Iconscout</a>
---
+++ 15_Rotate_Animation/vertexShader.glsl
@@ -0,0 +1,18 @@
+
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+
+out vec3 vertexColor;
+out vec2 texCoords;
+
+uniform mat4 transform;
+
+void main()
+{
+    gl_Position = transform * vec4(aPos, 1.0) ;
+    vertexColor = aColor;
+    texCoords = aTexCoord;
+}
+

9522ebb0b5dee5c665f991746fae7cd5dcbc8d22
Binulal Narayanan <binulal.narayanan@umontreal.ca>
2022-01-26 00:44:05 -0400

    3d cube without depth

---
+++ 17_3D_Cube/RenderWidget.py
@@ -0,0 +1,190 @@
+from PySide6.QtOpenGLWidgets import QOpenGLWidget
+from PySide6.QtOpenGL import (QOpenGLBuffer, QOpenGLVertexArrayObject)
+from PySide6.QtOpenGL import (QOpenGLShader, QOpenGLShaderProgram)
+from PySide6.QtOpenGL import (QOpenGLTexture)
+from PySide6.QtCore import Slot, QTimer
+from PySide6.QtGui import QOpenGLContext
+from PySide6.QtGui import QMatrix4x4
+from PySide6.QtGui import QImage
+
+from PySide6.support import VoidPtr
+
+from OpenGL import GL
+
+import numpy as np
+
+class RenderWidget(QOpenGLWidget):
+    def __init__(self, fmt):
+        super().__init__()
+        self.setFormat(fmt)
+        self.context = QOpenGLContext(self)
+        
+        self.program = None
+        self.angle = 0.0
+        self.timer = None
+        
+        if not self.context.create():
+            raise Exception("Unable to create GL context")
+        
+    def initializeGL(self):
+        # Set up the rendering context, load shaders and other resources, etc.:
+        f = self.context.functions()
+        f.glClearColor(0.2, 0.3, 0.3, 1.0);
+        #
+        # Load and compile shaders; and link program
+        #
+        self.program = QOpenGLShaderProgram(self)
+        self.program.addShaderFromSourceFile(QOpenGLShader.Vertex, "vertexShader.glsl")
+        self.program.addShaderFromSourceFile(QOpenGLShader.Fragment, "fragmentShader.glsl")
+        self.program.link()
+        #
+        # Setup VBO and VAO
+        #
+        vertices = np.array([
+            -0.5, -0.5, -0.5,  0.0, 0.0,
+             0.5, -0.5, -0.5,  1.0, 0.0,
+             0.5,  0.5, -0.5,  1.0, 1.0,
+             0.5,  0.5, -0.5,  1.0, 1.0,
+            -0.5,  0.5, -0.5,  0.0, 1.0,
+            -0.5, -0.5, -0.5,  0.0, 0.0,
+            
+            -0.5, -0.5,  0.5,  0.0, 0.0,
+             0.5, -0.5,  0.5,  1.0, 0.0,
+             0.5,  0.5,  0.5,  1.0, 1.0,
+             0.5,  0.5,  0.5,  1.0, 1.0,
+            -0.5,  0.5,  0.5,  0.0, 1.0,
+            -0.5, -0.5,  0.5,  0.0, 0.0,
+            
+            -0.5,  0.5,  0.5,  1.0, 0.0,
+            -0.5,  0.5, -0.5,  1.0, 1.0,
+            -0.5, -0.5, -0.5,  0.0, 1.0,
+            -0.5, -0.5, -0.5,  0.0, 1.0,
+            -0.5, -0.5,  0.5,  0.0, 0.0,
+            -0.5,  0.5,  0.5,  1.0, 0.0,
+            
+             0.5,  0.5,  0.5,  1.0, 0.0,
+             0.5,  0.5, -0.5,  1.0, 1.0,
+             0.5, -0.5, -0.5,  0.0, 1.0,
+             0.5, -0.5, -0.5,  0.0, 1.0,
+             0.5, -0.5,  0.5,  0.0, 0.0,
+             0.5,  0.5,  0.5,  1.0, 0.0,
+            
+            -0.5, -0.5, -0.5,  0.0, 1.0,
+             0.5, -0.5, -0.5,  1.0, 1.0,
+             0.5, -0.5,  0.5,  1.0, 0.0,
+             0.5, -0.5,  0.5,  1.0, 0.0,
+            -0.5, -0.5,  0.5,  0.0, 0.0,
+            -0.5, -0.5, -0.5,  0.0, 1.0,
+            
+            -0.5,  0.5, -0.5,  0.0, 1.0,
+             0.5,  0.5, -0.5,  1.0, 1.0,
+             0.5,  0.5,  0.5,  1.0, 0.0,
+             0.5,  0.5,  0.5,  1.0, 0.0,
+            -0.5,  0.5,  0.5,  0.0, 0.0,
+            -0.5,  0.5, -0.5,  0.0, 1.0
+        ],  dtype=np.float32 )
+
+        # Setup VAO
+        self.vao = QOpenGLVertexArrayObject()
+        vao_binder = QOpenGLVertexArrayObject.Binder(self.vao)
+
+        # Create and bind VBO
+        self.vbo = QOpenGLBuffer()
+        self.vbo.create()
+        self.vbo.bind()
+        # Allocate VBO, and copy in data
+        vertices_data = vertices.tobytes()
+        # self.vbo.allocate( data_to_initialize , data_size_to_allocate )
+        self.vbo.allocate( VoidPtr(vertices_data), 4 * vertices.size )
+        
+        # configure vertex attribute 0 and 1
+        # QOpenGLShaderProgram.setAttributeBuffer(location, type, offset, tupleSize[, stride=0])
+        self.program.setAttributeBuffer(0, GL.GL_FLOAT, 0, 3, 5 * vertices.itemsize )
+        self.program.setAttributeBuffer(1, GL.GL_FLOAT, 3 * vertices.itemsize, 2, 5 * vertices.itemsize )
+        self.program.enableAttributeArray(0)
+        self.program.enableAttributeArray(1)
+        
+        # Release VBO
+        self.vbo.release()
+        
+        # Setup texture1
+        self.texture1 = QOpenGLTexture(QImage("../images/container.jpg").mirrored())
+        self.texture1.setMinificationFilter(QOpenGLTexture.LinearMipMapLinear)
+        self.texture1.setMagnificationFilter(QOpenGLTexture.Linear)
+        # Setup texture2
+        self.texture2 = QOpenGLTexture(QImage("../images/awesomeface.png").mirrored())
+        self.texture2.setMinificationFilter(QOpenGLTexture.LinearMipMapLinear)
+        self.texture2.setMagnificationFilter(QOpenGLTexture.Linear)
+
+        # Start rotation
+        self.play()
+
+    def resizeGL(self, w, h):
+        # Update projection matrix and other size related settings:
+        f = self.context.functions()
+        retina_scale = self.devicePixelRatio()
+        f.glViewport(0, 0, self.width() * retina_scale, self.height() * retina_scale)
+    
+    def paintGL(self):
+        # Draw the scene:
+        f = self.context.functions()
+        f.glClear(GL.GL_COLOR_BUFFER_BIT)
+
+        # start painting
+        # Bind program
+        self.program.bind()
+       
+        # Setup texture locations
+        tex1 = self.program.uniformLocation("vTexture1")
+        self.program.setUniformValue(tex1, 0)
+        tex2 = self.program.uniformLocation("vTexture2")
+        self.program.setUniformValue(tex2, 1)
+ 
+        model = self.program.uniformLocation("model")
+        view = self.program.uniformLocation("view")
+        projection = self.program.uniformLocation("projection")
+
+        M_model = QMatrix4x4()
+        # Rotate -55 degrees around x axis 
+        M_model.rotate( self.angle, 0.5, 1.0, 0.0 )
+        
+        M_view  = QMatrix4x4()
+        M_view.translate( 0.0, 0.0, -3.0 )
+
+        M_projection = QMatrix4x4()
+        M_projection.perspective(45.0, 4.0 / 3.0, 0.1, 100.0)
+                
+        # Bind VAO
+        vao_binder = QOpenGLVertexArrayObject.Binder(self.vao)
+
+        # bind textures
+        self.texture1.bind(0)
+        self.texture2.bind(1)
+        
+        # update the transform matrix
+        self.program.setUniformValue(model, M_model)
+        self.program.setUniformValue(view, M_view)
+        self.program.setUniformValue(projection, M_projection)
+
+        f.glDrawArrays(GL.GL_TRIANGLES, 0, 36)
+
+        self.texture1.release()
+        self.texture2.release()
+        # Release program
+        self.program.release()
+
+    @Slot()
+    def updateAngle(self):
+        self.angle += 1.0
+        self.angle %= 360.0
+        self.update()
+
+    @Slot()
+    def play(self):
+        if self.timer is None:
+            self.timer = QTimer(self)
+            self.timer.timeout.connect(self.updateAngle)
+
+        if not self.timer.isActive():
+                self.timer.start(10)
+

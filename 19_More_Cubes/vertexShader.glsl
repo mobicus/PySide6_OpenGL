c8370eed6d7ccc4f66ca6527f0bf66b94a41bd35
Binulal Narayanan <binulal.narayanan@umontreal.ca>
2022-01-26 01:41:46 -0400

    more cubes

---
+++ 19_More_Cubes/vertexShader.glsl
@@ -0,0 +1,19 @@
+#version 330 core
+
+layout (location = 0) in vec3 vPosition; 
+layout (location = 1) in vec2 vTexCoord;
+
+out vec2 texCoords;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    gl_Position = projection * view * model * vec4(vPosition, 1.0);
+    texCoords = vTexCoord;
+}
+
+
+
